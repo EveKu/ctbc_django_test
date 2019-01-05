05a47ee6b71b645df1e0e70d834feed8cd8482e7
eveku <eveku@data-sci.info>
2019-01-06 01:31:40 +0800

    add_hash

--- model_summary/models.py
+++ model_summary/models.py
@@ -7,6 +7,8 @@ class ModelSummary(models.Model):
     target_symbol = models.CharField(max_length=10, null=True, blank=True)
     loss = models.FloatField(null=True, blank=True)
     accuracy = models.FloatField(null=True, blank=True)
+    model_path = models.CharField(max_length=100, null=True, blank=True)
+    checksum = models.CharField(max_length=255, editable=False, unique=True, null=True)
     last_modify_date = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
